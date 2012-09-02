fda914813dacd5596eef4591d2bb8488c363a0aa
uu59 <k@uu59.org>
2012-09-02 17:03:27 +0900

    remove HOME constant, add #home method
    it was auto-created .grill directory when `require "grill"`,
    but it's not testable and too agressive.

--- lib/grill.rb
+++ lib/grill.rb
@@ -7,23 +7,26 @@ module Grill
   class << self
     attr_reader :gemfile
 
-    HOME = "#{ENV["HOME"] || "."}/.grill"
-    unless File.directory?(HOME)
-      FileUtils.mkdir(HOME)
+    def home
+      home = "ENV["HOME"] || "."}/.grill"
+      unless File.directory?(home)
+        FileUtils.mkdir_p(home)
+      end
+      home
     end
 
     def implant(gems)
       fullpath = File.expand_path($0)
-      gemfile = "#{HOME}/#{Digest::MD5.hexdigest(fullpath)}"
+      gemfile = "#{home}/#{Digest::MD5.hexdigest(fullpath)}"
       @gem = gems
       tmp = File.open(gemfile, "w")
       tmp.puts @gem
       tmp.close
       ENV["BUNDLE_GEMFILE"] = tmp.path
 
-      unless system(%Q!bundle check --gemfile "#{gemfile}" --path #{HOME}/gems > #{File::NULL}!)
+      unless system(%Q!bundle check --gemfile "#{gemfile}" --path #{home}/gems > #{File::NULL}!)
         puts "missing gems found. will install them"
-        system(%Q!bundle install --gemfile "#{gemfile}" --path "#{HOME}/gems"!)
+        system(%Q!bundle install --gemfile "#{gemfile}" --path "#{home}/gems"!)
         puts "gems are installed."
       end
       require "bundler/setup"
