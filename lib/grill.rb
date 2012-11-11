681d46ea8659d58da800726468a8964b2c3b8bca
uu59 <k@uu59.org>
2012-11-11 15:30:31 +0900

    Separate each gemfile with a ruby engines

--- lib/grill.rb
+++ lib/grill.rb
@@ -4,12 +4,22 @@ require "grill/version"
 
 module Grill
   class << self
+
     def home
       home = "#{ENV["GRILL_HOME"] || ENV["HOME"] || "."}/.grill"
       unless File.directory?(home)
         FileUtils.mkdir_p(home)
       end
       home
+    end
+
+    def gemfile_path(gems)
+      fullpath = File.expand_path($0)
+      filename = "#{Digest::MD5.hexdigest(fullpath)}-#{Digest::MD5.hexdigest(gems)}"
+      path = File.join(home, ruby, filename)
+      dir = File.dirname(path)
+      FileUtils.mkdir_p(dir) unless File.directory?(dir)
+      path
     end
 
     def implant(*args)
@@ -23,21 +33,26 @@ module Grill
         gems << "\n"
       end
 
-      fullpath = File.expand_path($0)
-      gemfile = "#{home}/#{Digest::MD5.hexdigest(fullpath)}-#{Digest::MD5.hexdigest(gems)}"
+      gemfile = gemfile_path(gems)
       tmp = File.open(gemfile, "w")
       tmp.puts gems
       tmp.close
       ENV["BUNDLE_GEMFILE"] = tmp.path
 
-      unless system(%Q!bundle check --gemfile "#{gemfile}" --path #{home}/gems > #{File::NULL}!)
+      gemdir = File.join(home, ruby)
+      unless system(%Q!bundle check --gemfile "#{gemfile}" --path "#{gemdir}" > #{File::NULL}!)
         puts "missing gems found. will install them"
-        system(%Q!bundle install --gemfile "#{gemfile}" --path "#{home}/gems"!)
+        system(%Q!bundle install --gemfile "#{gemfile}" --path "#{gemdir}"!)
         puts "gems are installed."
       end
 
       require "bundler/setup"
       Bundler.require
+    end
+
+    private
+    def ruby
+      "#{defined?(RUBY_ENGINE) ? RUBY_ENGINE : "ruby18"}-#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}"
     end
   end
 end
