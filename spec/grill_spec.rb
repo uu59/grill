3260308505b5fb313810847d57bc89735ed3c0ee
uu59 <k@uu59.org>
2013-07-10 23:24:11 +0900

    Refactoring spec

--- spec/grill_spec.rb
+++ spec/grill_spec.rb
@@ -15,52 +15,54 @@ describe Grill do
     Grill.home.should match %r!^#{ENV["GRILL_HOME"]}/.grill!
   end
 
-  # cannot use `context` because test will be broken
-  # I don't know why but maybe that `context` require Bundler
-  it "#implant by String" do
-    cleanroom do
-      defined?(Dummy).should be_nil
+  context "#implant" do
+    it "by String" do
+      cleanroom do
+        defined?(Dummy).should be_nil
 
-      Grill.implant(<<-GEM)
-        gem "dummy", :path => "#{File.expand_path(".././support/dummy", __FILE__)}"
-      GEM
-      defined?(Dummy).should_not be_nil
-    end
-  end
-
-  it "#implant by Symbol" do
-    cleanroom do
-      name = :test
-      File.open("#{Grill.home}/#{name}", "w") do |f|
-        f.puts <<-GEM
+        Grill.implant(<<-GEM)
           gem "dummy", :path => "#{File.expand_path(".././support/dummy", __FILE__)}"
-          gem "dummy2", :path => "#{File.expand_path(".././support/dummy2", __FILE__)}"
         GEM
+        defined?(Dummy).should_not be_nil
       end
-      defined?(Dummy).should be_nil
-      defined?(Dummy2).should be_nil
-
-      Grill.implant name
-      defined?(Dummy).should_not be_nil
-      defined?(Dummy2).should_not be_nil
     end
+
+    it "by Symbol" do
+      cleanroom do
+        name = :test
+        File.open("#{Grill.home}/#{name}", "w") do |f|
+          f.puts <<-GEM
+            gem "dummy", :path => "#{File.expand_path(".././support/dummy", __FILE__)}"
+            gem "dummy2", :path => "#{File.expand_path(".././support/dummy2", __FILE__)}"
+          GEM
+        end
+        defined?(Dummy).should be_nil
+        defined?(Dummy2).should be_nil
+
+        Grill.implant name
+        defined?(Dummy).should_not be_nil
+        defined?(Dummy2).should_not be_nil
+      end
+    end
+
+    it "multiple" do
+      cleanroom do
+        defined?(Dummy).should be_nil
+        defined?(Dummy2).should be_nil
+
+        Grill.implant <<-FOO, <<-BAR
+        gem "dummy", :path => "#{File.expand_path(".././support/dummy", __FILE__)}"
+        FOO
+        gem "dummy2", :path => "#{File.expand_path(".././support/dummy2", __FILE__)}"
+        BAR
+
+        defined?(Dummy).should_not be_nil
+        defined?(Dummy2).should_not be_nil
+      end
+    end
+
   end
 
-  it "#implant multiple" do
-    cleanroom do
-      defined?(Dummy).should be_nil
-      defined?(Dummy2).should be_nil
-
-      Grill.implant <<-FOO, <<-BAR
-      gem "dummy", :path => "#{File.expand_path(".././support/dummy", __FILE__)}"
-      FOO
-      gem "dummy2", :path => "#{File.expand_path(".././support/dummy2", __FILE__)}"
-      BAR
-
-      defined?(Dummy).should_not be_nil
-      defined?(Dummy2).should_not be_nil
-    end
-  end
 
   it "separete Gemfile different contants" do
     gemfile1 = 'gem "hogehoge"'
